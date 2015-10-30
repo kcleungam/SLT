6f7788b530c62ef26424eef8609939693ff5cab1
kcleungam <kcleungam@ust.hk>
2015-10-30 21:06:48 +0800

    Create slt.java

---
+++ slt.java
@@ -0,0 +1,28 @@
+import com.leapmotion.leap.*;
+
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.io.Console;
+import java.util.Scanner;
+import java.util.Vector;
+import java.lang.Math;
+import java.util.*;
+
+
+public class SLT {
+    Listener listener = new Listener();//Create New Listener For The Leap
+    Controller controller = new Controller();//Create New Controller For The Leap
+
+    Frame frame = controller.frame();
+    Finger finger = frame.fingers().get(arg0);
+    ScreenList screenList = controller.calibratedScreens();
+    Screen screen = screenList.get(0);
+
+    com.leapmotion.leap.Vector inter = screen.intersect(finger, true);
+    com.leapmotion.leap.Vector sped = finger.tipVelocity();
+    int sx = screen.widthPixels();
+    int sy = screen.heightPixels();
+
+}
