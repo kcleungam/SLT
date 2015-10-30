69c2177a6bd6988a8a5d5df39e99d58e21793f3b
skyluke712 <skyluke712@gmail.com>
2015-10-31 00:01:03 +0800

    data inserting and database schema is added

--- database.java
+++ database.java
@@ -21,22 +21,14 @@ public class database {
     MongoClient mongoClient = new MongoClient();
     MongoDatabase db = mongoClient.getDatabase("test");
     
-    //DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
-    
     //example of data inserting and the database schema
     db.getCollection("testing_gesture").insertOne(
             new Document("sign_ID", "001")
                     .append("meaning", "Hello")
                     .append("part_of_speech", "")
                     .append("frames", asList(
-//                            new Document()
-//                                    .append("date", format.parse("2014-10-01T00:00:00Z"))
-//                                    .append("grade", "A")
-//                                    .append("score", 11),
-//                            new Document()
-//                                    .append("date", format.parse("2014-01-16T00:00:00Z"))
-//                                    .append("grade", "B")
-//                                    .append("score", 17)))
+                    		/*Frames*/
+                    		)
                     .append("country", "Hong Kong")	//maybe optional
                     .append("type", "single_handed"));
 }
