55744d8000aecb480990193b3f4170c2a56aae1b
ylcheungab <ylcheungab@connect.ust.hk>
2016-01-15 23:10:45 +0800

    Testing tmr... mod. needed

--- Visualizer.java
+++ Visualizer.java
@@ -1,63 +1,48 @@
-import java.applet.Applet;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GraphicsConfiguration;
-import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Container;
-
-import javax.media.j3d.AmbientLight;
-import javax.media.j3d.Appearance;
-import javax.media.j3d.BoundingSphere;
-import javax.media.j3d.BranchGroup;
-import javax.media.j3d.Canvas3D;
-import javax.media.j3d.ColoringAttributes;
-import javax.media.j3d.DirectionalLight;
-import javax.media.j3d.Material;
-import javax.media.j3d.Transform3D;
-import javax.media.j3d.TransformGroup;
-import javax.swing.Timer;
-import javax.vecmath.Color3f;
-import javax.vecmath.Point3d;
-import javax.vecmath.Vector3f;
-
-import com.sun.j3d.utils.applet.MainFrame;
-import com.sun.j3d.utils.geometry.Sphere;
-import com.sun.j3d.utils.universe.SimpleUniverse;
-
 /*
  * https://java3d.java.net/binary-builds.html
  * use the link above for the java 3d libraries,
  * vector math library included.
- * -Jacky
+ * - Jacky
  */
 
-public class Visualizer extends Applet{
+import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.GraphicsConfiguration;
+import javax.media.j3d.AmbientLight;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.DirectionalLight;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.HandList;
+import com.leapmotion.leap.Vector;
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.geometry.Sphere;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+
+public class Visualizer extends Applet implements Runnable {
 	
 	private SimpleUniverse u;
 	private BranchGroup scene;
-	
-	private TransformGroup objTrans;
-	
+	private TransformGroup objGroup = new TransformGroup();
 	private Canvas3D c;
-		
-	private double height = 0.0f;
 	
-	private double width = 0.0f;
+	private Controller lmcontrol;
 	
-	private Sphere lf1 = new Sphere(0.1f);
-	private Sphere lf2 = new Sphere(0.1f);
-	private Sphere lf3 = new Sphere(0.1f);
-	private Sphere lf4 = new Sphere(0.1f);
-	private Sphere lf5 = new Sphere(0.1f);
-	private Sphere lp = new Sphere(0.2f);
-	private Sphere rf1 = new Sphere(0.1f);
-	private Sphere rf2 = new Sphere(0.1f);
-	private Sphere rf3 = new Sphere(0.1f);
-	private Sphere rf4 = new Sphere(0.1f);
-	private Sphere rf5 = new Sphere(0.1f);
-	private Sphere rp = new Sphere(0.2f);
+	// finger tips, follows the LM setting
+	// i.e, 0 = left, 1 = right
+	// 0 = thumb , etc
+	private TransformGroup[][] fingers = new TransformGroup[2][5];
+	
+	// palms
+	private TransformGroup[] palms = new TransformGroup[2] ;
 
 
 	public Visualizer() {
@@ -66,9 +51,50 @@ public class Visualizer extends Applet{
 	        .getPreferredConfiguration();
 	    Canvas3D c = new Canvas3D(config);
 	    add("Center", c);
-	    // Create a simple scene and attach it to the virtual universe
+	    // Create a simple scene...
 	    scene = createSceneGraph();
+	    scene.addChild(objGroup);
+	    
+	    for (int i = 0; i < 2; i ++){
+	    	for(int j = 0; j < 5; j ++){
+	    		fingers[i][j] = new TransformGroup();
+	    		fingers[i][j].addChild(new Sphere(0.1f));
+	    	}
+	    	palms[i] = new TransformGroup();
+	    	palms[i].addChild(new Sphere(0.2f));
+	    }
+	    
+	    lmcontrol = new Controller();
+	    // and attach it to the virtual universe
+	    u = new SimpleUniverse(c);
+	    u.getViewingPlatform().setNominalViewingTransform();
+	    u.addBranchGraph(scene);
+	}
+	
+	public Visualizer(Controller controller) {
+	    setLayout(new BorderLayout());
+	    GraphicsConfiguration config = SimpleUniverse
+	        .getPreferredConfiguration();
+	    Canvas3D c = new Canvas3D(config);
+	    add("Center", c);
+	    // Create a simple scene
+	    scene = createSceneGraph();
+	    scene.addChild(objGroup);
+	    
+	    for (int i = 0; i < 2; i ++){
+	    	for(int j = 0; j < 5; j ++){
+	    		fingers[i][j] = new TransformGroup();
+	    		fingers[i][j].addChild(new Sphere(0.1f));
+	    		fingers[i][j].setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+	    	}
+	    	palms[i] = new TransformGroup();
+	    	palms[i].addChild(new Sphere(0.2f));
+	    	palms[i].setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+	    }
+	    
+	    lmcontrol = controller;
 
+	    // Attach the simple scene to the virtual universe
 	    u = new SimpleUniverse(c);
 	    u.getViewingPlatform().setNominalViewingTransform();
 	    u.addBranchGraph(scene);
@@ -77,13 +103,11 @@ public class Visualizer extends Applet{
 	public BranchGroup createSceneGraph() {
 		// Create the root of the branch graph
 	    BranchGroup objRoot = new BranchGroup();
-	    objTrans = new TransformGroup();
+	    TransformGroup objTrans = new TransformGroup();
 	    objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 	    objRoot.addChild(objTrans);
 
 	    // Create a simple shape leaf node, add it to the scene graph.
-	    Vector3f vec = new Vector3f(0.0f, 0.0f, 0.0f);
-	    objRoot.addChild(updateFingerTip(vec));
 	    BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0),
 	        100.0);
 
@@ -108,29 +132,16 @@ public class Visualizer extends Applet{
 	 * if has the sphere, add it/ translate it
 	 * if no , delete it
 	 */
-	public TransformGroup updateFingerTip(Vector3f coordinate){
+	public void updateSphere(Vector coordinate, TransformGroup translate){
 		Sphere sphere = new Sphere(0.25f);
-		/*
-		Appearance ap = new Appearance();	// in Java3D, u must set the color through Appearance 
-		Material material = new Material();	// use material for 3d look
+		Vector3f graphvec = new Vector3f(coordinate.getX(), coordinate.getY(), coordinate.getZ());
 		
-		if (hand == "left"){
-			material = new Material(new Color3f(Color.red), new Color3f(Color.black), new Color3f(Color.red), new Color3f(Color.white), 70f);
-		}else if(hand == "right"){
-			material = new Material(new Color3f(Color.cyan), new Color3f(Color.black), new Color3f(Color.cyan), new Color3f(Color.white), 70f);
-		}else{
-			return null;
-		}
-		ap.setMaterial(material);
-		sphere.setAppearance(ap);
-		*/
-		objTrans = new TransformGroup();
-	    objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 	    Transform3D pos1 = new Transform3D();
-	    pos1.setTranslation(coordinate);
-	    objTrans.setTransform(pos1);
-	    objTrans.addChild(sphere);
-		return objTrans;
+	    pos1.setTranslation(graphvec);
+	    objGroup.setTransform(pos1);
+	    if (objGroup.indexOfChild(translate) == -1){
+	    	objGroup.addChild(sphere);
+	    }
 	}
 	
 	/*
@@ -144,5 +155,47 @@ public class Visualizer extends Applet{
 	public static void main(String[] args) {
 		Visualizer bb = new Visualizer();
 	    MainFrame mf = new MainFrame(bb, 256, 256);
+	}
+
+	public void traceLM(){
+		while(true){
+			Frame frame = lmcontrol.frame();
+			HandList hands = frame.hands();
+			if (hands.count() > 1) {
+                for (int i = 0; i < 2 ; i ++){
+                	for (int j = 0 ; j < 5 ; j ++){
+                    	updateSphere(hands.get(i).fingers().get(j).tipPosition(), fingers[i][j]);
+                	}
+                	updateSphere(hands.get(i).palmPosition(), palms[i]);
+                }
+            } else if (hands.count() > 1){
+            	if (hands.get(0).isLeft()) {
+                	for (int j = 0 ; j < 5 ; j ++){
+                    	updateSphere(hands.get(0).fingers().get(j).tipPosition(), fingers[0][j]);
+                	}
+                	updateSphere(hands.get(0).palmPosition(), palms[0]);
+                } else{
+                	for (int j = 0 ; j < 5 ; j ++){
+                    	updateSphere(hands.get(1).fingers().get(j).tipPosition(), fingers[1][j]);
+                	}
+                	updateSphere(hands.get(0).palmPosition(), palms[1]);
+                }
+            } else {
+            	clear();
+            }
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			// may insert the play here...
+		}
+	}
+	
+	@Override
+	public void run() {
+		traceLM();
 	}
 }
