9369429f24e5146c44b4f916d8cf23c0d5e80bbd
Fire-Prometheus <alextowunyin@gmail.com>
2016-02-02 01:18:36 +0800

    Re-organised
    Re-organised with the newly separated classes and enum. Changed some
    field names as well.

--- Sample.java
+++ Sample.java
@@ -18,97 +18,14 @@ import java.util.Collection;
 
 public class Sample {
 
+    /**
+     * inner class
+     */
+
     class OneFrame{
         FingerData fingerData;
         PalmData palmData;
         HandType handType;
-
-        class FingerData{
-            public int count = 0;
-            public ArrayList<Coordinate> coordinates = new ArrayList<>();
-
-            public FingerData(){};
-
-            public FingerData(Frame frame){
-
-                HandList first=frame.hands();
-
-                //the code should be safe as Leap Motion controller won't recognise more than 2 hands
-                if (first.count() > 1) {
-                    handType = HandType.BOTH;
-                } else if (first.get(0).isLeft()) {
-                    handType = HandType.LEFT;
-                } else{
-                    handType = HandType.RIGHT;
-                }
-
-                count=0;
-                if(handType == HandType.BOTH){
-                    for(Hand hand:first){
-                        for (Finger finger : hand.fingers())
-                            if (finger.isExtended()) count++;
-                    }
-                }else{
-                    // doesn't work when hands.get(0) change to hand(0)
-                    for (Finger finger : first.get(0).fingers())
-                        if (finger.isExtended())
-                            count++;
-                }
-
-                //iterate finger by finger to get the coordinates of the finger(s)
-                // If single hand, put it directly
-                // otherwise, put finger in left hand first( position 0-4) and then right hand (position 5-9)
-                if (frame.fingers().count() > 0) {
-                    if(frame.fingers().count() == 5){
-                        for (Finger finger : frame.fingers()) {
-                            Coordinate cor = new Coordinate(finger.tipPosition().getX(), finger.tipPosition().getY(), finger.tipPosition().getZ());
-                            coordinates.add(cor);
-                        }
-                    }else if(frame.fingers().count() == 10){
-                        int label = 0;
-                        for(Hand hand : frame.hands()){
-                            if(hand.isLeft()){
-                                label = 0;
-                            }else {
-                                label = 5;
-                            }
-                            for(Finger finger : hand.fingers()){
-                                Coordinate cor = new Coordinate(finger.tipPosition().getX(), finger.tipPosition().getY(), finger.tipPosition().getZ());
-                                coordinates.set(label,cor);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        class PalmData{
-            public int count = 0;
-            public ArrayList<Coordinate> coordinates = new ArrayList<>();
-
-            public PalmData(Frame frame) {
-                this.count = frame.hands().count();
-                if (this.count > 0) {
-                    if(this.count == 1){
-                        for (Hand hand : frame.hands()) {
-                            Coordinate cor = new Coordinate(hand.palmPosition().getX(), hand.palmPosition().getY(), hand.palmPosition().getZ());
-                            coordinates.add(cor);
-                        }
-                    }else if(this.count == 2){
-                        for(Hand hand : frame.hands()){
-                            Coordinate cor = new Coordinate(hand.palmPosition().getX(), hand.palmPosition().getY(), hand.palmPosition().getZ());
-                            if(hand.isLeft()){
-                                coordinates.set(0, cor);
-                            }else{
-                                coordinates.set(1, cor);
-                            }
-                        }
-                    }
-
-                }
-
-            }
-        }
 
         public OneFrame(){}
 
@@ -119,108 +36,20 @@ public class Sample {
 
             fingerData = new FingerData(frame);
             palmData = new PalmData(frame);
+            this.handType=fingerData.handType;
         }
     }
 
-    /* inner class */
-    /*
-    class FingerData{
-        public int count;
-        public ArrayList< ArrayList<Coordinate> > coordinateSeq=new ArrayList<>();// sequence of coordinates frame by frame
 
-        //default constructor
-        public FingerData(){}
-        //preferred constructor
-        public FingerData(ArrayList<Frame> source){
-            //count the number of fingers in the first frame
-            HandList first=source.get(0).hands();
-            count=0;
-            if(allHands.type==HandType.BOTH){
-                for(Hand hand:first){
-                    for (Finger finger : first.get(0).fingers())
-                        if (finger.isExtended()) count++;
-                }
-            }else{
-                // doesn't work when hands.get(0) change to hand(0)
-                for (Finger finger : first.get(0).fingers())
-                    if (finger.isExtended())
-                        count++;
-            }
-
-            //iterate frame by frame to get the coordinates of the finger(s)
-            for (Frame frame : source) {
-                ArrayList<Coordinate> fingers_coordinates = new ArrayList<>();
-                if (frame.fingers().count() > 0) {
-                    for (Finger finger : frame.fingers()) {
-                        Coordinate cor = new Coordinate(finger.tipPosition().getX(), finger.tipPosition().getY(), finger.tipPosition().getZ());
-                        fingers_coordinates.add(cor);
-                    }
-                    coordinateSeq.add(fingers_coordinates);
-                }
-            }
-        }
-    }
-
-    class PalmData{
-        public ArrayList< ArrayList<Coordinate> > coordinateSeq=new ArrayList<>();// sequence of coordinates frame by frame
-
-        //default constructor
-        public PalmData(){}
-        //preferred constructor
-        public PalmData(ArrayList<Frame> source){
-            for (Frame frame : source) {
-                ArrayList<Coordinate> palm_coordinates = new ArrayList<>();
-                if (frame.hands().count() > 0) {
-                    for (Hand hand : frame.hands()) {
-                        Coordinate cor = new Coordinate(hand.palmPosition().getX(), hand.palmPosition().getY(), hand.palmPosition().getZ());
-                        palm_coordinates.add(cor);
-                    }
-                    coordinateSeq.add(palm_coordinates);
-                }
-            }
-        }
-    }
-
-    class HandData{
-        public HandType type;
-        public int count;
-
-        //default constructor
-        public HandData(){}
-        //preferred constructor
-        public HandData(ArrayList<Frame> source){
-            //determine the characteristic of hand(s) by the first frame
-            HandList first=source.get(0).hands();
-
-            //the code should be safe as Leap Motion controller won't recognise more than 2 hands
-            count=first.count();
-            if (this.count > 1) {
-                type=HandType.BOTH;
-            } else if (first.get(0).isLeft()) {
-                type=HandType.LEFT;
-            } else{
-                type=HandType.RIGHT;
-            }
-        }
-    }
-
-    */
 
     /**
      * field
      */
 
-    /*
-    FingerData allFingers;
-    PalmData allPalms;
-    HandData allHands;
-    */
-
-
-    static enum HandType{LEFT,RIGHT,BOTH}
-    ArrayList<OneFrame> allFrame;
-    int fingerCount = 0;
-    HandType handType;
+    public ArrayList<OneFrame> allFrame=new ArrayList<>();
+    public int initialFingerCount = 0;
+    public int initialPalmCount=0;
+    public HandType initialHandType;
 
 
     /**
@@ -232,14 +61,7 @@ public class Sample {
 	Use the constructor forcing you to give initialised parameter instead.
 	 */
     // for Jongo exclusively
-    public Sample() {
-        /*
-        allFingers = new FingerData();
-        allPalms = new PalmData();
-        allHands=new HandData();
-        */
-        allFrame = new ArrayList<>();
-    }
+    public Sample() {}
 
     //copy constructor
     public Sample(Sample source) {
@@ -254,22 +76,16 @@ public class Sample {
         //become the Array List type
         ArrayList<Frame> frames= new ArrayList<Frame>();frames.addAll(source);
 
-        /*
-        allHands=new HandData(frames);
-        allPalms=new PalmData(frames);
-        allFingers=new FingerData(frames);
-        */
-
         for(Frame frame : frames){
             if(frame == null){
                 throw new Exception();
             }
-            OneFrame oneFrame = new OneFrame(frame);
-            this.allFrame.add(oneFrame);
+            this.allFrame.add(new OneFrame(frame));
         }
 
-        this.fingerCount = this.allFrame.get(0).fingerData.count;
-        this.handType = this.allFrame.get(0).handType;
+        initialFingerCount = this.allFrame.get(0).fingerData.count;
+        initialPalmCount=this.allFrame.get(0).palmData.count;
+        initialHandType = this.allFrame.get(0).handType;
     }
 }
 
